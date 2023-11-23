86a6cbbb18128fcbaac12849b7a27db1753b770e
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2023-11-23 18:28:49 +0530

    Add files via upload

---
+++ app/Account/loading.js
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center my-32'><div className="loadingio-spinner-spinner-qquvqbfosq"><div className="ldio-7yavza0ogx9">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div></div>
+  )
+}
