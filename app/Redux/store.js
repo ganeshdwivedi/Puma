7cb0b3c0ecc1e8e34790dc08b67ebc6e8c306ef7
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2023-11-24 20:50:43 +0530

    Update store.js

--- app/Redux/store.js
+++ app/Redux/store.js
@@ -1,13 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import cartReducer from "./cartSlice"
 import wishlistReducer from './wishlistSlice'
-import authReducer from './authSlice'
 
 const store = configureStore({
     reducer:{
        cart:cartReducer,
        wishlist:wishlistReducer,
-       auth:authReducer
+    
     }
 })
 export default store
